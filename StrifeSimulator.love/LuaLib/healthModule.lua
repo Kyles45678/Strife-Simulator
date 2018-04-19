2126652957c3769168feb63ae8c607cb9116eebe
aocampo2862 <aocampo2862@gmail.com>
2018-04-19 08:58:27 -0500

    damage function

--- StrifeSimulator.love/LuaLib/healthModule.lua
+++ StrifeSimulator.love/LuaLib/healthModule.lua
@@ -1,6 +1,8 @@
 local healthModuleName = {}
 healthModule = healthModuleName
 
+local environmentModule = require("LuaLib.environmentModule")
+
 local healthBarWidth
 local playerLives
 local playerX
@@ -9,15 +11,26 @@ function healthModule.load(player)
 
 end
 
+function healthModule.damage(player, damage)
+	player.health = player.health - damage
+end
+
 function healthModule.update(dt, player)
 	healthBarWidth = player.health * 4
 	playerLives = player.lives
+
+	if player.health <= 0 then
+		playerLives = playerLives - 1
+		player.health = 8
+	end
 end
 
 function healthModule.display(player)
 	--display a health bar above the player
 	love.graphics.setColor(255, 70, 70)
 	love.graphics.rectangle('fill', player.hurtBox.Position.X, player.hurtBox.Position.Y - 25, healthBarWidth, 10)
+
+	--display player health
 	love.graphics.setColor(255, 255, 55)
 	if player.playerIndex == 1 then
 		love.graphics.print("Player " .. tostring(player.playerIndex) .. " Lives: " .. tostring(playerLives), 100, 15)
