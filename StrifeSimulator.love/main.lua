bc5663e3b85fadc97405afcdddf896f38874a20d
MasterMemeMinister <soupcateatternom@gmail.com>
2018-04-30 01:04:15 -0500

    Fullscreen broken pls nerf

--- StrifeSimulator.love/main.lua
+++ StrifeSimulator.love/main.lua
@@ -13,7 +13,7 @@ local player2
 function love.load() 
 	gameModule.load()
 
-	helperModule.load()
+	--helperModule.load()
 
 	player1 = playerModule.Player:new("Andrew", constantsModule.player1up, constantsModule.player1down, constantsModule.player1left, constantsModule.player1right, constantsModule.player1attack, love.graphics.getWidth() * (3/8), love.graphics.getHeight() * (3/4), 1) 
 	player2 = playerModule.Player:new("Biggie Smalls", constantsModule.player2up, constantsModule.player2down, constantsModule.player2left, constantsModule.player2right, constantsModule.player2attack, love.graphics.getWidth() * (5/8) - 32, love.graphics.getHeight() * (3/4), 2)  
